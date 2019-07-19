452dbb562e0549494dee45041bce768f8be296f4
G-Mustafa <ghulammustafa137@gmail.com>
2019-07-19 17:00:58 +0500

    robofriends v2

---
+++ src/SearchBox.js
@@ -0,0 +1,13 @@
+import React,{ Component } from 'react';
+
+class SearchBox extends Component {
+    render() {
+        return (
+            <div className='pa2'>
+                <input onChange={this.props.searchChange} className='pa3 ba b--green bg-lightest-blue' type='search' placeholder='Search Robots' />
+            </div>
+        );
+    }
+}
+
+export default SearchBox;
