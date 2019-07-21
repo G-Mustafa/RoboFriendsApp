8b58fa7d1b0e72e7fb849b908af0d7731e73b245
G-Mustafa <ghulammustafa137@gmail.com>
2019-07-21 13:46:57 +0500

    Updates

---
+++ precache-manifest.36ef32326ab960acaf504f266b64f24c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1e26909ce00c99f03ff695b14a0cb04",
+    "url": "/RoboFriendsApp/index.html"
+  },
+  {
+    "revision": "6f6d9243dc53ff0713d5",
+    "url": "/RoboFriendsApp/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "bb1d7c8d6365de8da9d8",
+    "url": "/RoboFriendsApp/static/css/main.56aac3ff.chunk.css"
+  },
+  {
+    "revision": "6f6d9243dc53ff0713d5",
+    "url": "/RoboFriendsApp/static/js/2.2d717d87.chunk.js"
+  },
+  {
+    "revision": "bb1d7c8d6365de8da9d8",
+    "url": "/RoboFriendsApp/static/js/main.f9e3d86c.chunk.js"
+  },
+  {
+    "revision": "40cbab871f78f77db683",
+    "url": "/RoboFriendsApp/static/js/runtime~main.9967c245.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RoboFriendsApp/static/media/SEGA.12a3db8b.woff"
+  }
+]);
